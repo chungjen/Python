a4d3b8c964601bb2829a64ab05de11433a82bb07
chungjen <31300309+chungjen@users.noreply.github.com>
2020-05-24 18:05:26 +0800

    Add files via upload

---
+++ Loops1.py
@@ -0,0 +1,6 @@
+
+states = ["New York", "Michigan", "California", "Texas", "Oregon"]
+
+for state in states:
+	  print(state)
+	  
---
+++ Loops2.py
@@ -0,0 +1,12 @@
+
+abbrevs = {
+    "New York" : "NY",
+    "Michigan" : "MI", 
+    "California" : "CA", 
+    "Texas" : "TX",
+    "Oregon" : "OR"
+}
+
+for key in abbrevs:
+    print(key + ":" + abbrevs[key])
+
