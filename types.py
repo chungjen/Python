a6cd255041d1b131b90f364525880d535b7b13a1
chungjen <31300309+chungjen@users.noreply.github.com>
2020-05-24 15:23:36 +0800

    Add files via upload

---
+++ types.py
@@ -0,0 +1,20 @@
+firstname = "Ben"
+lastname = "Finkel"
+age = 41
+married = True
+height = 68.7
+
+# print(age * 20)
+
+# print(firstname * 20)
+
+print("The type pf the firstname variable is: " + str(type(firstname)))
+
+# firstname = 200
+
+# print("The type pf the firstname variable is: " + str(type(firstname)))
+
+print("The type pf the age variable is: " + str(type(age)))
+
+print("The type pf the married variable is: " + str(type(married)))
+
